bf47deb47b6f0d45ab0da63d4c9957992265e01d
Ricardo Pablo <De Leon>
2018-03-22 07:36:28 +0800

    Implement search functionality in MovieSearchApp

--- MovieSearchApp/app.js
+++ MovieSearchApp/app.js
@@ -3,11 +3,23 @@ var app = express();
 
 var request = require("request");
 
+// Setup ejs
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+    res.render("search");
+});
+
 app.get("/results", function(req, res) {
-    request("http://www.omdbapi.com/?apikey=thewdb&s=california", function(error, response, body) {
+    var query = req.query.search;
+    var url = "http://www.omdbapi.com/?apikey=thewdb&s=" + query;
+    console.log(url + query);
+    request(url, function(error, response, body) {
         if (!error && response.statusCode == 200) {
-            var parsedData = JSON.parse(body);    
-            res.send(parsedData["Search"][0]["Title"]);
+
+            // Parse data then pass to ejs
+            var data = JSON.parse(body);    
+            res.render("results", {data: data});
         }    
     });
 });
