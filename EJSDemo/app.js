eb2024581fde1667ff93346386f911d54d4122a2
Ricardo Pablo <De Leon>
2018-03-21 14:54:17 +0800

    Update EJS files to serve custom assets

--- EJSDemo/app.js
+++ EJSDemo/app.js
@@ -1,12 +1,19 @@
-var app = require("express")();
+var express = require("express")
+var app = express();
+
+// serve public folder
+app.use(express.static("public"));
+
+// use ejs
+app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/fallinlovewith/:thing", function(req, res) {
     var thing = req.params.thing;
-    res.render("love.ejs", {thingVar: thing});
+    res.render("love", {thingVar: thing});
 });
 
 app.get("/posts", function(req, res) {
@@ -15,7 +22,7 @@ app.get("/posts", function(req, res) {
         {title: "My adorable pet bunny", author: "Max"},
         {title: "Can you believe it?", author: "Jill"},
     ]
-    res.render("posts.ejs", {posts: posts});
+    res.render("posts", {posts: posts});
 });
 
 app.listen(3000, function() {
