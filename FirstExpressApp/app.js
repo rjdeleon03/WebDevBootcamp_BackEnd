f6b55e74c0341a90decb71c177aa8f4c703bdd54
Ricardo Pablo <De Leon>
2018-03-21 10:30:23 +0800

    Add basic express and package.json exercises

---
+++ FirstExpressApp/app.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var app = express();
+
+// "/" => "Hi there!"
+app.get("/", function(req, res) {
+    res.send("Hi there!");
+});
+
+// "/bye" => "Goodbye!"
+app.get("/bye", function(req, res) {
+    res.send("Goodbye!");
+});
+
+// "/dog" =? "MEOW!"
+app.get("/dog", function(req, res) {
+    console.log("SOMEONE MADE A REQUEST TO /DOG!!!");
+    res.send("MEOW!");
+});
+
+app.get("/r/:subredditName/", function(req, res) {
+    console.log(req.params);
+    var subreddit = req.params.subredditName;
+    res.send("WELCOME TO THE " + subreddit.toUpperCase() + " SUBREDDIT!");
+});
+
+app.get("/r/:subredditName/comments/:id/:title", function(req, res) {
+    console.log(req.params);
+    res.send("WELCOME TO THE COMMENTS PAGE!");
+});
+
+// catch-all
+app.get("*", function(req, res) {
+    res.send("YOU ARE A STAR!!!");
+});
+
+// Tell express to listen
+app.listen(3000, function() {
+    console.log("Serving first express app on port 3000");
+});
