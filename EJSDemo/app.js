2fff921ec634bc9915bb4f8147f0aca0f14d90c9
Ricardo Pablo <De Leon>
2018-03-21 12:50:47 +0800

    Add EJSDemo files

---
+++ EJSDemo/app.js
@@ -0,0 +1,23 @@
+var app = require("express")();
+
+app.get("/", function(req, res) {
+    res.render("home.ejs");
+});
+
+app.get("/fallinlovewith/:thing", function(req, res) {
+    var thing = req.params.thing;
+    res.render("love.ejs", {thingVar: thing});
+});
+
+app.get("/posts", function(req, res) {
+    var posts = [
+        {title: "Post 1", author: "Susy"},
+        {title: "My adorable pet bunny", author: "Max"},
+        {title: "Can you believe it?", author: "Jill"},
+    ]
+    res.render("posts.ejs", {posts: posts});
+});
+
+app.listen(3000, function() {
+    console.log("Server is listening!");
+});
