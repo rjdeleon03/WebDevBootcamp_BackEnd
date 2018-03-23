3d3afd5b04385dcc4b7b3a70b8b1e210d30323f5
Ricardo Pablo <De Leon>
2018-03-23 11:43:44 +0800

    Implement SHOW routing in blog demo

--- RESTfulBlog/app.js
+++ RESTfulBlog/app.js
@@ -52,6 +52,17 @@ app.post("/blogs", function(req, res) {
     });
 });
 
+// SHOW ROUTE
+app.get("/blogs/:id", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog) {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
+
 // LISTEN TO PORT 3000
 app.listen(3000, function() {
     console.log("RESTful Blog is being served...");
